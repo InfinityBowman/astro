29f66c36aedda36b2cfbe5fe8aab6ef955c69225
Jacob Maynard <JacobAMaynard@proton.me>
2025-02-23 19:46:12 -0700

    try build again

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,27 +1,27 @@
-import pkg from "pg";
-const { Client } = pkg;
+// import pkg from "pg";
+// const { Client } = pkg;
 
-export async function GET({ request }) {
-  try {
-    const client = new Client({
-      host: "172.21.219.188",
-      user: "jacobmaynard",
-      password: "dumbInfinity42$",
-      database: "vst_rating_db",
-    });
+// export async function GET({ request }) {
+//   try {
+//     const client = new Client({
+//       host: "172.21.219.188",
+//       user: "jacobmaynard",
+//       password: "dumbInfinity42$",
+//       database: "vst_rating_db",
+//     });
 
-    await client.connect();
-    const res = await client.query("SELECT * FROM your_table");
-    await client.end();
+//     await client.connect();
+//     const res = await client.query("SELECT * FROM your_table");
+//     await client.end();
 
-    return new Response(JSON.stringify(res.rows), {
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    console.error("Database query error:", error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-}
+//     return new Response(JSON.stringify(res.rows), {
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     console.error("Database query error:", error);
+//     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
